1862b7cc225f028133f24bbca12e21b826895077
AbdiN7 <55746065+AbdiN7@users.noreply.github.com>
2019-10-31 14:37:37 -0400

    test

--- src/main/java/com/ss/lms/LmsApplication.java
+++ src/main/java/com/ss/lms/LmsApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class LmsApplication {
-		//testing webhook
+		//testing webhook 2
 	public static void main(String[] args) {
 		System.out.println("Hello there");
 		SpringApplication.run(LmsApplication.class, args);
